3b56b6d728636a754d42fc4a07e7637c977e7588
PanshulK <>
2018-10-14 12:13:25 +0530

    code upload

--- SearchBarDemo/ViewController.swift
+++ SearchBarDemo/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        searchBar.showsCancelButton = true
+        
+    }
+    
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar)
+    {
+        print("text begin editing");
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // Do some search stuff
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        // Stop doing the search stuff
+        // and clear the text in the search bar
+        searchBar.text = ""
+        // Hide the cancel button
+        searchBar.showsCancelButton = true
+        // You could also change the position, frame etc of the searchBar
+        
+        
+        print("cancel button");
     }
 
     override func didReceiveMemoryWarning() {
